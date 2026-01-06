e3d63381bd586a2fac0ef03291a27b8fbed154e7
Rushikengar8686 <kengar.rushi@gmail.com>
2026-01-06 14:50:55 +0530

    Changes Done

--- reactcourse/src/Topics/SetState.js
+++ reactcourse/src/Topics/SetState.js
@@ -2,7 +2,17 @@ import React, { use, useState } from 'react';
 
 const SetState = () => {
     //create Student Information 
-    const[studentInfo,setStudentInfo] = useState({StudeName:"",Mark:"",Grade:"" })
+    const [studentInfo, setStudentInfo] = useState({ StudeName: "", Mark: "", Grade: "" })
+
+    // add studetn list
+    const [studetnList, setStudetnList] = useState([])
+
+    // add student list in table 
+    const addStudentList = () => {
+        setStudetnList(prevObject => ([...prevObject, studentInfo]))
+        setStudentInfo({ StudeName: "", Mark: "", Grade: "" })
+    }
+
     // First way to acces value in input box and append to object
     // const onchangeStudentName = (event)=>{
     //     setStudentInfo(prevObject => ({...prevObject,StudeName:event.target.value}))
@@ -15,24 +25,24 @@ const SetState = () => {
     // }
 
     // Now second way
-    const onchageStudentInfo = (event,key)=>{
-        setStudentInfo(prevObject =>({...prevObject,[key]:event.target.value}))
+    const onchageStudentInfo = (event, key) => {
+        setStudentInfo(prevObject => ({ ...prevObject, [key]: event.target.value }))
     }
 
     // Now if we want to add city in arrayList 
-    const [cityList,setCityList] = useState(["Pune","Mumbai"])
-    
+    const [cityList, setCityList] = useState(["Pune", "Mumbai"])
+
     // to hold input textbox value in this variable
-    const [cityName,setCityName]  = useState([])
-    
-    const printCityName = (event)=>{
+    const [cityName, setCityName] = useState([])
+
+    const printCityName = (event) => {
         setCityName(event.target.value)
     }
 
 
     // add More City in array
-    const addCity = ()=>{
-        setCityList(prevObject => [...prevObject,cityName])
+    const addCity = () => {
+        setCityList(prevObject => [...prevObject, cityName])
         setCityName()
     }
     return (
@@ -50,31 +60,56 @@ const SetState = () => {
                         <input type="text" className='form-control' onChange={(event)=> {onchageStudentGrade(event)}} placeholder='Enter Student Grade' />
                     </div>
                 </div> */}
-                 <div className="row">
+                <div className="row">
                     <div className="col-3">
-                        <input type="text" className='form-control'onChange={(event)=>{onchageStudentInfo(event,"StudeName")}} placeholder='Enter Student Name' />
+                        <input type="text" className='form-control' onChange={(event) => { onchageStudentInfo(event, "StudeName") }} placeholder='Enter Student Name' />
                     </div>
-                     <div className="col-3">
-                        <input type="text" className='form-control' onChange={(event)=>{onchageStudentInfo(event,"Mark")}} placeholder='Enter Student Mark' />
+                    <div className="col-3">
+                        <input type="text" className='form-control' onChange={(event) => { onchageStudentInfo(event, "Mark") }} placeholder='Enter Student Mark' />
                     </div>
-                     <div className="col-3">
-                        <input type="text" className='form-control' onChange={(event)=>{onchageStudentInfo(event,"Grade")}} placeholder='Enter Student Grade' />
+                    <div className="col-3">
+                        <input type="text" className='form-control' onChange={(event) => { onchageStudentInfo(event, "Grade") }} placeholder='Enter Student Grade' />
                     </div>
-                     <div className="col-3">
-                        <button type="button" className='btn btn-primary'>Save</button>
+                    <div className="col-3">
+                        <button type="button" className='btn btn-primary' onClick={addStudentList}> Save</button>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-12 mt-3">
+                        <table className='table table-bordered table-hover text-center'>
+                            <thead>
+                                <tr>
+                                    <th>Sr.No</th>
+                                <th>Student Name</th>
+                                <th>Student Mark</th>
+                                <th>Student Grade</th>
+                                </tr>
+                            </thead>
+
+                            <tbody>
+                                {studetnList.map((item, index) => (
+                                    <tr key={index}>
+                                        <td>{index + 1}</td>
+                                        <td>{item.StudeName}</td>
+                                        <td>{item.Mark}</td>
+                                        <td>{item.Grade}</td>
+                                    </tr>
+                                ))}
+                            </tbody>
+                        </table>
                     </div>
                 </div>
                 <div className="row mt-3">
                     <h5>Task Second</h5>
                     <div className="col-4">
-                        <input type="text" name="" id=""  onChange={(event)=>{printCityName(event)}} className='form-control' placeholder='Enter City Name'/>
+                        <input type="text" name="" id="" onChange={(event) => { printCityName(event) }} className='form-control' placeholder='Enter City Name' />
                     </div>
                     <div className="col-4">
                         <button type="button" className='btn btn-success' onClick={addCity}>Add City</button>
                     </div>
                     <div className="col-4">
-                       {cityList.map((item)=> <h2>{item}</h2>
-                    )}
+                        {cityList.map((item) => <h2>{item}</h2>
+                        )}
                     </div>
                 </div>
             </div>
